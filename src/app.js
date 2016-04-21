19579fd23f3ec981ec03e8a77b8fed9c1413ec4c
Eric Tsai <erictsai6@gmail.com>
2016-04-21 00:15:36 -0400

    Added in tumblrwks npm package and created POST route that will send the proper API call to Tumblr to create a new post

--- src/app.js
+++ src/app.js
@@ -1,14 +1,36 @@
 var express = require('express');
 var config = require('./config/config');
-var tumblr = require('tumblr');
+var Tumblr = require('tumblrwks');
 var logger = require('./utils/logger');
+var http = require('http');
+
+var tumblr = new Tumblr({
+  consumerKey: config.consumer_key,
+  consumerSecret: config.consumer_secret,
+  accessToken: config.token,
+  accessSecret: config.token_secret
+}, 'grubhub-umami.tumblr.com');
 
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+// all environments
+app.set('port', config.port);
+
+app.post('/v1/hipchat', function(req, res) {
+    tumblr.post('/post', {
+        type: 'text',
+        title: 'tumblrwkstesting',
+        body: '<h3>should work!! </h3>'
+    })
+    .then(function(json) {
+        res.send();
+    });
 });
 
-app.listen(config.port, function () {
-    logger.info('TIL server listening on port 3000!');
+app.get('/', function (req, res) {
+    res.send('Health check');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+    logger.info('TIL server listening on port %d!', config.port);
 });
