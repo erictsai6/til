2ffa3707dd0a8bedc9c45691596888ff3fddc929
Eric Tsai <etsai@grubhub.com>
2016-04-21 10:09:04 -0400

    Actually writes a blog post taking into account the user information, Future TODO will be to consolidate all the same messages of the same day into a single blog post

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ var config = require('./config/config');
 var Tumblr = require('tumblrwks');
 var logger = require('./utils/logger');
 var http = require('http');
+var bodyParser = require('body-parser');
+var moment = require('moment');
 
 var tumblr = new Tumblr({
   consumerKey: config.consumer_key,
@@ -15,20 +17,36 @@ var app = express();
 
 // all environments
 app.set('port', config.port);
+app.use(bodyParser.json())
 
 app.post('/v1/hipchat', function(req, res) {
 
-    console.log(req.body);
+
+    var author = req.body.item.message.from.name;
+    var message = req.body.item.message.message;
+    var datetime = moment(req.body.item.message.date);
 
     tumblr.post('/post', {
         type: 'text',
-        title: 'tumblrwksddtesting',
-        body: '<h3>shoulddd work!! </h3>'
+        title: datetime.format('ddd, MMM DD YYYY'),
+        body: author + ': ' + message
     }, function(err, json) {
         if (err) {
             console.log(err);
+            res.json({
+                "color": "yellow",
+                "message": "TIL failed to upload your entry",
+                "notify": true,
+                "message_format": "text"
+            });
+        } else {
+            res.json({
+                "color": "green",
+                "message": "TIL thanks you for your contribution",
+                "notify": false,
+                "message_format": "text"
+            });
         }
-        res.send();
     });
 });
 
